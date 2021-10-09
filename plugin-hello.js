697c123b8edbd4dc8d2ee3922dc1f2d4a54e1575
AngelBeats-Kanade <angelbeats177@gamil.com>
2021-10-09 16:24:09 +0800

    Update pixiv

--- plugin-hello.js
+++ plugin-hello.js
@@ -4,6 +4,6 @@ const { bot } = require("./index")
 // hello world
 // 向你的机器人发送 hello
 bot.on("message", function (e) {
-	if (e.raw_message === "hello")
-		e.reply("hello world")
+	if (e.raw_message.startsWith("hello") || e.raw_message.startsWith("你好"))
+		e.reply("你好！")
 })
