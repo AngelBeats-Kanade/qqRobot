55e6f3ac8a035c0ddf81982b0e5988aa9849b91a
Angelbeats-Kanade <angelbeats177@gamil.com>
2021-10-29 13:02:13 +0800

    Update pixiv

--- plugin-pixiv.js
+++ plugin-pixiv.js
@@ -60,9 +60,7 @@ function getRandomInt(min, max) {
 }
 
 function getDailyPicture(e, r18) {
-    var randomPage = getRandomInt(1, 10)
-    if (r18)
-        randomPage = getRandomInt(1, 2)
+    var randomPage = r18 ? getRandomInt(1, 2) : getRandomInt(1, 10)
     var randomNumber = getRandomInt(0, 49)
 
     getDailyPictureByNumber(e, r18, randomPage, randomNumber)
@@ -71,14 +69,119 @@ function getDailyPicture(e, r18) {
 function getDailyPictureByNumber(e, r18, page, number) {
     var rank = ((page - 1) * 50 + number).toString()
     var title = r18 ? dailyNumberR18Collection[rank] : dailyNumberCollection[rank]
-    var url = r18 ? dailyR18Pictures[title]["url"] : dailyPictures[title]["url"]
 
+    fetchAPictureAndReply(title, r18, e)
+}
+
+function getDailyPictureByTag(e, r18, tag) {
+    var tagCollection = r18 ? dailyTagR18Collection : dailyTagCollection
+    if (tag in tagCollection) {
+        var randomNumber = getRandomInt(0, tagCollection[tag].length - 1)
+        var title = tagCollection[tag][randomNumber]
+
+        fetchAPictureAndReply(title, r18, e)
+    } else {
+        var randomPage = getRandomInt(1, 10)
+        var url = r18 ? `https://www.pixiv.net/ajax/search/artworks/${tag}?word=${tag}&order=date_d&mode=r18&p=${randomPage}&s_mode=s_tag&type=all` : `https://www.pixiv.net/ajax/search/artworks/${tag}?word=${tag}&order=date_d&mode=all&p=${randomPage}&s_mode=s_tag&type=all`
+        fetch(url, {
+            headers: {
+                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
+                'referer': 'https://www.pixiv.net/ranking.php?mode=daily&content=illust',
+                'cookie': 'first_visit_datetime_pc=2021-09-28+15:52:57; p_ab_id=2; p_ab_id_2=3; p_ab_d_id=954436876; yuid_b=dHWVURA; device_token=50f27ce2596f15db46485386e2658f1b; privacy_policy_agreement=3; c_type=20; privacy_policy_notification=0; a_type=0; b_type=2; d_type=4,1; login_ever=yes; tag_view_ranking=liM64qjhwQ~uGQeWvelyQ~VqqXyMy80A~sYhl4SsLi1~RTJMXD26Ak~Z-FJ6AMFu8~EZQqoW9r8g~2R7RYffVfj~y9yFdNjFJo~MSNRmMUDgC~-98s6o2-Rp~hLwBbPEXzz~tgP8r-gOe_~qXzcci65nj~FDo7nPJEjf~8kCkoI701J~jH0uD88V6F~-sp-9oh8uv~i4Q_o7CyIB~eVxus64GZU~aKhT3n4RHZ~1Jc1EqM6Ff~jk9IzfjZ6n~0xsDLqCEW6~vMp-NoNmIL~QKeXYK2oSR~qWFESUmfEs~jsyFBENSMj~pvP44gGKdO~DADQycFGB0~PKOnf9fn03~jsuXqE_4cM~3WPZidqT9B~Ged1jLxcdL~WVrsHleeCL~Ie2c51_4Sp~ouiK2OKQ-A~DbUnfvXURp~C9_ZtBtMWU~gpglyfLkWs~SzTIWVCj2t~pWwa6Fh3R4~M0gtANhbpW~faHcYIP1U0~dJgU3VsQmO~NBK37t_oSE~Rq7EdadiWT~y8GNntYHsi~BU9SQkS-zU~fmZgEP1p5s~K8esoIs2eW~MHugbgF9Xo~So7otvWMNl~yPNaP3JSNF~4ZEPYJhfGu~fg8EOt4owo~FfFuZRxXNV~afM5Sp3Id1~b1s-xqez0Y~DpO7Lofslr~Lt-oEicbBr~F5CBR92p_Q~vti3o9ERHH~t9yCQU2bWx~55h6mysr_-~81BOcT1ZAV~R3lr4__Kr8~ILoPAjoYZ5~gooMLQqB9a~xZ6jtQjaj9~HY55MqmzzQ~ZRGAWQ4_eJ~ZZltVrbyeV~SkAFAh85DH~3gc3uGrU1V~E8vkum0JtI~AWJIXzMXQa~aMoljgmCaZ~kGYw4gQ11Z~QaiOjmwQnI~0rsCr94LAC~C1zxl77dvd~ThSueiucQX~o5DB__cIwt~nhVUm2hb1U~6GYRfMzuPl~UCT8y2nU0w~dGedGTSL3b~QTtzgGH2pR~_uueDi_8NC~VV3Uu--0IH~OZbzcrhaSe~l-nkIKv2D0~31XfoCHwdp~QliAD3l3jr~LLyDB5xskQ~TWrozby2UO~GOuKuI1rXg~MI2kUkwUjZ~w0A5rVRfvZ; __cf_bm=bmrKQhxY6ldMPGwdwGjc0aA1lFc.hHbO6M3qLOs9Gew-1634749480-0-AbPsFPrveyhtJAXJ2ZtVIW8GhZO8Pg1pzKmczdKLyu1XwnwkMZ5aGcfogcXVETkM1dEE/x0MrhG8MtYqX462TlzisrpMEkUsA5/PgOzyqOFMnM0IR2FOtdsk35+7mQ29vmanKdlHT1cfklkU7scX8GC6IrGnWtbRbqxEthD658RwhTHzMKluEuixtrqPbV358w==; PHPSESSID=24223512_EaWTPfyfrk5ZXfcqWCAiywocwa069bU1'
+            }
+        }).then(data => data.json())
+            .then(function (data) {
+                var head = r18 ? '色图来了！嘿嘿嘿～' : '二次元图片来了！'
+                let picture = ''
+                let title = ''
+                let url = ''
+                let page = ''
+                let uid = ''
+                let id = ''
+                let user = ''
+                let tags = ''
+                let temp = 0
+                let promiseArr = []
+                for (let j = 0; j < 50; j++) {
+                    let p = new Promise((resolve, reject) => {
+                        fetch(`https://www.pixiv.net/artworks/${data["body"]["illustManga"]["data"][j]["id"]}`)
+                            .then(data => data.text())
+                            .then(function (text) {
+                                let bookMark = parseInt(text.match(/"bookmarkCount":(.+?),"likeCount"/)[1])
+                                if (bookMark > temp) {
+                                    if (!r18 && (data["body"]["illustManga"]["data"][j]["tags"].indexOf('R-18') != -1 || data["body"]["illustManga"]["data"][j]["tags"].indexOf('R-18G') != -1)) { } else {
+                                        temp = bookMark
+                                        picture = text.match(/"original":"(.+?)"},"tags"/)[1]
+                                        title = data["body"]["illustManga"]["data"][j]["title"]
+                                        url = `https://www.pixiv.net/artworks/${data["body"]["illustManga"]["data"][j]["id"]}`
+                                        page = data["body"]["illustManga"]["data"][j]["pageCount"]
+                                        uid = data["body"]["illustManga"]["data"][j]["userId"]
+                                        id = data["body"]["illustManga"]["data"][j]["id"]
+                                        user = data["body"]["illustManga"]["data"][j]["userName"]
+                                        tags = data["body"]["illustManga"]["data"][j]["tags"]
+                                    }
+                                }
+                                resolve()
+                            })
+                    })
+                    promiseArr.push(p)
+                }
+                Promise.all(promiseArr).then(res => {
+                    if (page === 1) {
+                        let reply = `${head}\n作者：${user}\nuid：${uid}\ntitle：${title}\ntags：${tags}\np站链接：${url}\n国内直连链接：https://pixiv.re/${id}` + picture.substr(-4, 4)
+                        e.reply(
+                            [
+                                reply
+                            ]
+                        )
+                        e.reply(
+                            [
+                                segment.image(`https://pixiv.cat/${id}` + picture.substr(-4, 4))
+                            ]
+                        ).then(function (results) {
+                            if (r18) {
+                                setTimeout(function () {
+                                    bot.deleteMsg(results["data"]["message_id"])
+                                }, 10000)
+                            }
+                        })
+                    } else {
+                        let reply = `${head}\n作者：${user}\nuid：${uid}\ntitle：${title}\ntags：${tags}\np站链接：${url}\n国内直连链接：`
+                        for (var i = 1; i <= page; i++) {
+                            reply += `https://pixiv.re/${id}-${i}` + picture.substr(-4, 4)
+                            reply += `\n`
+                        }
+                        e.reply(
+                            [
+                                reply
+                            ]
+                        )
+                        for (var i = 1; i <= page; i++) {
+                            e.reply(
+                                [
+                                    segment.image(`https://pixiv.cat/${id}-${i}` + picture.substr(-4, 4))
+                                ]
+                            ).then(function (results) {
+                                if (r18) {
+                                    setTimeout(function () {
+                                        bot.deleteMsg(results["data"]["message_id"])
+                                    }, 10000)
+                                }
+                            })
+                        }
+                    }
+                })
+            })
+    }
+}
+
+function fetchAPictureAndReply(title, r18, e) {
+    var url = r18 ? dailyR18Pictures[title]["url"] : dailyPictures[title]["url"]
     fetch(url)
         .then(text => text.text())
         .then(function (text) {
             var picture = text.match(/"original":"(.+?)"},"tags"/)[1]
             var head = r18 ? '色图来了！嘿嘿嘿～' : '二次元图片来了！'
-            var url = r18 ? dailyR18Pictures[title]["url"] : dailyPictures[title]["url"]
             var page = r18 ? dailyR18Pictures[title]["pages"] : dailyPictures[title]["pages"]
             var uid = r18 ? dailyR18Pictures[title]["uid"] : dailyPictures[title]["uid"]
             var id = r18 ? dailyR18Pictures[title]["id"] : dailyPictures[title]["id"]
@@ -128,126 +231,6 @@ function getDailyPictureByNumber(e, r18, page, number) {
                 }
             }
         })
-}
-
-function getDailyPictureByTag(e, r18, tag) {
-    if (!r18) {
-        if (tag in dailyTagCollection) {
-            var randomNumber = getRandomInt(0, dailyTagCollection[tag].length - 1)
-            var title = dailyTagCollection[tag][randomNumber]
-
-            fetch(dailyPictures[title]["url"])
-                .then(text => text.text())
-                .then(function (text) {
-                    var picture = text.match(/"original":"(.+?)"},"tags"/)[1]
-                    var url = dailyPictures[title]["url"]
-                    var page = dailyPictures[title]["pages"]
-                    var uid = dailyPictures[title]["uid"]
-                    var id = dailyPictures[title]["id"]
-                    var user = dailyPictures[title]["user"]
-                    var tags = dailyPictures[title]["tags"]
-                    var reply = `二次元图片来啦！\n作者：${user}\nuid：${uid}\ntitle：${title}\ntags：${tags}\np站链接：${url}\n国内直连链接：https://pixiv.re/${id}` + picture.substr(-4, 4)
-                    if (page === '1') {
-                        e.reply(
-                            [
-                                reply
-                            ]
-                        )
-                        e.reply(
-                            [
-                                segment.image(`https://pixiv.cat/${id}` + picture.substr(-4, 4))
-                            ]
-                        )
-                    } else {
-                        reply = `二次元图片来啦！\n作者：${user}\nuid：${uid}\ntitle：${title}\ntags：${tags}\np站链接：${url}\n国内直连链接：`
-                        for (var i = 1; i <= page; i++) {
-                            reply += `https://pixiv.re/${id}-${i}` + picture.substr(-4, 4)
-                            reply += `\n`
-                        }
-                        e.reply(
-                            [
-                                reply
-                            ]
-                        )
-                        for (var i = 1; i <= page; i++) {
-                            e.reply(
-                                [
-                                    segment.image(`https://pixiv.cat/${id}-${i}` + picture.substr(-4, 4))
-                                ]
-                            )
-                        }
-                    }
-                })
-        } else {
-            e.reply(
-                [
-                    '抱歉刀客塔，阿米娅没有在今天的排行榜上找到这个tag！请换个tag试试！想要从罗德岛得到帮助，请说“阿米娅，告诉我怎么用tag搜索”或“.help tag”'
-                ]
-            )
-        }
-    } else {
-        if (tag in dailyTagR18Collection) {
-            var randomNumber = getRandomInt(0, dailyTagR18Collection[tag].length - 1)
-            var title = dailyTagR18Collection[tag][randomNumber]
-
-            fetch(dailyR18Pictures[title]["url"])
-                .then(text => text.text())
-                .then(function (text) {
-                    var picture = text.match(/"original":"(.+?)"},"tags"/)[1]
-                    var url = dailyR18Pictures[title]["url"]
-                    var page = dailyR18Pictures[title]["pages"]
-                    var uid = dailyR18Pictures[title]["uid"]
-                    var id = dailyR18Pictures[title]["id"]
-                    var user = dailyR18Pictures[title]["user"]
-                    var tags = dailyR18Pictures[title]["tags"]
-                    var reply = `色图来啦！\n作者：${user}\nuid：${uid}\ntitle：${title}\ntags：${tags}\np站链接：${url}\n国内直连链接：https://pixiv.re/${id}` + picture.substr(-4, 4)
-                    if (page === '1') {
-                        e.reply(
-                            [
-                                reply
-                            ]
-                        )
-                        e.reply(
-                            [
-                                segment.image(`https://pixiv.cat/${id}` + picture.substr(-4, 4))
-                            ]
-                        ).then(function (results) {
-                            setTimeout(function () {
-                                bot.deleteMsg(results["data"]["message_id"])
-                            }, 10000)
-                        })
-                    } else {
-                        reply = `色图来啦！\n作者：${user}\nuid：${uid}\ntitle：${title}\ntags：${tags}\np站链接：${url}\n国内直连链接：`
-                        for (var i = 1; i <= page; i++) {
-                            reply += `https://pixiv.re/${id}-${i}` + picture.substr(-4, 4)
-                            reply += `\n`
-                        }
-                        e.reply(
-                            [
-                                reply
-                            ]
-                        )
-                        for (var i = 1; i <= page; i++) {
-                            e.reply(
-                                [
-                                    segment.image(`https://pixiv.cat/${id}-${i}` + picture.substr(-4, 4))
-                                ]
-                            ).then(function (results) {
-                                setTimeout(function () {
-                                    bot.deleteMsg(results["data"]["message_id"])
-                                }, 10000)
-                            })
-                        }
-                    }
-                })
-        } else {
-            e.reply(
-                [
-                    '抱歉刀客塔，阿米娅没有在今天的排行榜上找到这个tag！请换个tag试试！想要从罗德岛得到帮助，请说“阿米娅，告诉我怎么用tag搜索”或“.help tag”'
-                ]
-            )
-        }
-    }
 }
 
 function fetchData() {
