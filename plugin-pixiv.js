4855ecd53c521950fcaa24f95d9c4217a4badb03
Angelbeats-Kanade <angelbeats177@gamil.com>
2021-11-13 02:04:03 +0800

    Update pixiv

--- plugin-pixiv.js
+++ plugin-pixiv.js
@@ -232,6 +232,44 @@ function fetchAPictureAndReply(title, r18, e) {
         })
 }
 
+function fetchDataByRandomTag(randomTag, r18) {
+    let randomTagCollection = []
+    for (let i = 1; i <= 10; i++) {
+        let url = r18 ? `https://www.pixiv.net/ajax/search/artworks/${randomTag}?word=${randomTag}&order=date_d&mode=r18&p=${i}&s_mode=s_tag&type=all` : `https://www.pixiv.net/ajax/search/artworks/${randomTag}?word=${randomTag}&order=date_d&mode=all&p=${i}&s_mode=s_tag&type=all`
+        fetch(url, {
+            headers: {
+                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
+                'referer': 'https://www.pixiv.net/ranking.php?mode=daily&content=illust',
+                'cookie': 'first_visit_datetime_pc=2021-09-28+15:52:57; p_ab_id=2; p_ab_id_2=3; p_ab_d_id=954436876; yuid_b=dHWVURA; device_token=50f27ce2596f15db46485386e2658f1b; privacy_policy_agreement=3; c_type=20; privacy_policy_notification=0; a_type=0; b_type=2; d_type=4,1; login_ever=yes; tag_view_ranking=liM64qjhwQ~uGQeWvelyQ~VqqXyMy80A~sYhl4SsLi1~RTJMXD26Ak~Z-FJ6AMFu8~EZQqoW9r8g~2R7RYffVfj~y9yFdNjFJo~MSNRmMUDgC~-98s6o2-Rp~hLwBbPEXzz~tgP8r-gOe_~qXzcci65nj~FDo7nPJEjf~8kCkoI701J~jH0uD88V6F~-sp-9oh8uv~i4Q_o7CyIB~eVxus64GZU~aKhT3n4RHZ~1Jc1EqM6Ff~jk9IzfjZ6n~0xsDLqCEW6~vMp-NoNmIL~QKeXYK2oSR~qWFESUmfEs~jsyFBENSMj~pvP44gGKdO~DADQycFGB0~PKOnf9fn03~jsuXqE_4cM~3WPZidqT9B~Ged1jLxcdL~WVrsHleeCL~Ie2c51_4Sp~ouiK2OKQ-A~DbUnfvXURp~C9_ZtBtMWU~gpglyfLkWs~SzTIWVCj2t~pWwa6Fh3R4~M0gtANhbpW~faHcYIP1U0~dJgU3VsQmO~NBK37t_oSE~Rq7EdadiWT~y8GNntYHsi~BU9SQkS-zU~fmZgEP1p5s~K8esoIs2eW~MHugbgF9Xo~So7otvWMNl~yPNaP3JSNF~4ZEPYJhfGu~fg8EOt4owo~FfFuZRxXNV~afM5Sp3Id1~b1s-xqez0Y~DpO7Lofslr~Lt-oEicbBr~F5CBR92p_Q~vti3o9ERHH~t9yCQU2bWx~55h6mysr_-~81BOcT1ZAV~R3lr4__Kr8~ILoPAjoYZ5~gooMLQqB9a~xZ6jtQjaj9~HY55MqmzzQ~ZRGAWQ4_eJ~ZZltVrbyeV~SkAFAh85DH~3gc3uGrU1V~E8vkum0JtI~AWJIXzMXQa~aMoljgmCaZ~kGYw4gQ11Z~QaiOjmwQnI~0rsCr94LAC~C1zxl77dvd~ThSueiucQX~o5DB__cIwt~nhVUm2hb1U~6GYRfMzuPl~UCT8y2nU0w~dGedGTSL3b~QTtzgGH2pR~_uueDi_8NC~VV3Uu--0IH~OZbzcrhaSe~l-nkIKv2D0~31XfoCHwdp~QliAD3l3jr~LLyDB5xskQ~TWrozby2UO~GOuKuI1rXg~MI2kUkwUjZ~w0A5rVRfvZ; __cf_bm=bmrKQhxY6ldMPGwdwGjc0aA1lFc.hHbO6M3qLOs9Gew-1634749480-0-AbPsFPrveyhtJAXJ2ZtVIW8GhZO8Pg1pzKmczdKLyu1XwnwkMZ5aGcfogcXVETkM1dEE/x0MrhG8MtYqX462TlzisrpMEkUsA5/PgOzyqOFMnM0IR2FOtdsk35+7mQ29vmanKdlHT1cfklkU7scX8GC6IrGnWtbRbqxEthD658RwhTHzMKluEuixtrqPbV358w==; PHPSESSID=24223512_EaWTPfyfrk5ZXfcqWCAiywocwa069bU1'
+            }
+        }).then(data => data.json())
+            .then(function (data) {
+                let promiseArr = []
+                for (let n = 0; n < 50; n++) {
+                    let p = new Promise((resolve, reject) => {
+                        dailyPictures[data["contents"][n]["title"]] = {}
+                        dailyPictures[data["contents"][n]["title"]]["url"] = `https://www.pixiv.net/artworks/${data["contents"][n]["illust_id"]}`
+                        dailyPictures[data["contents"][n]["title"]]["user"] = data["contents"][n]["user_name"]
+                        dailyPictures[data["contents"][n]["title"]]["uid"] = data["contents"][n]["user_id"]
+                        dailyPictures[data["contents"][n]["title"]]["id"] = data["contents"][n]["illust_id"]
+                        dailyPictures[data["contents"][n]["title"]]["pages"] = data["contents"][n]["illust_page_count"]
+                        dailyPictures[data["contents"][n]["title"]]["tags"] = data["contents"][n]["tags"]
+                        dailyNumberCollection[data["contents"][n]["rank"]] = data["contents"][n]["title"]
+                        let tags = data["contents"][n]["tags"]
+                        for (let j = 0; j < tags.length; j++) {
+                            if (!(tags[j] in dailyTagCollection))
+                                dailyTagCollection[tags[j]] = []
+                            dailyTagCollection[tags[j]].push(data["contents"][n]["title"])
+                        }
+                        resolve()
+                    })
+                    promiseArr.push(p)
+                }
+            })
+    }
+    return randomTagCollection
+}
+
 function fetchData() {
     for (var i = 1; i <= 10; i++) {
         var url = `https://www.pixiv.net/ranking.php?mode=daily&content=illust&p=${i}&format=json`
