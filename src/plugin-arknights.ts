b0422b1da8af772619cb4653d7ee1c66b1a3cc35
AngelBeats-Kanade <angelbeats177@gamil.com>
2022-04-23 01:12:26 +0800

    Update 2022/04/23

--- src/plugin-arknights.ts
+++ src/plugin-arknights.ts
@@ -1,29 +1,48 @@
-import { segment } from 'oicq';
 import { bot } from './plugin-bot';
 import * as fs from 'fs';
-import * as time from 'console';
+import * as schedule from 'node-schedule';
 
 let players: IPlayers;
 const arknightsData = 'data/arknights.json';
 
 initialize();
+schedule.scheduleJob('refreshDataEveryDay', '0 0 0 * * *', function () {
+    let key: (keyof IPlayers);
+
+    for (key in players) {
+        players[key].dailyPulls = 0;
+    }
+
+    const data = JSON.stringify(players, null, 2);
+
+    fs.writeFile(arknightsData, data, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log('JSON data is saved.');
+    });
+});
 
 bot.on('message', function (e) {
+    let name = e.sender.nickname;
+
     if (e.raw_message.search('阿米娅垫刀') != -1) {
         let number = e.raw_message.replace(/[^0-9]/ig, '');
-        let name = e.sender.nickname;
 
         if (!(name in players)) {
             players[name].name = name;
-            players[name].sixStars = 0
-            players[name].fiveStars = 0
-            players[name].totalPulls = 0
-            players[name].prePulls = 0 //指抽出货之前的垫刀，我实在不知道怎么翻译
-            players[name].dailyPulls = 0
-            players[name].probability = '2%'
+            players[name].sixStars = 0;
+            players[name].fiveStars = 0;
+            players[name].totalPulls = 0;
+            players[name].prePulls = 0; //指抽出货之前的垫刀，我实在不知道怎么翻译
+            players[name].dailyPulls = 0;
+            players[name].probability = '2%';
+            // players[name] = new Player(name);
         }
 
         players[name].prePulls += (number === '') ? 1 : parseInt(number);
+        players[name].dailyPulls += (number === '') ? 1 : parseInt(number);
+        players[name].totalPulls += (number === '') ? 1 : parseInt(number);
         players[name].probability = getProbability(players[name].prePulls);
 
         const data = JSON.stringify(players, null, 2);
@@ -37,7 +56,37 @@ bot.on('message', function (e) {
         let reply = `${name}当前的垫刀数为：${players[name].prePulls}\n当前6星概率为：${players[name].probability}\n今日已抽卡：${players[name].dailyPulls}\n累计共抽取了：${players[name].totalPulls}\n累计获得了${players[name].sixStars}个六星和${players[name].fiveStars}个五星`;
         e.reply(reply);
     }
-    if (e.raw_message.search('阿米娅设置') != -1) { }
+    if (e.raw_message.search('阿米娅设置') != -1) {
+        const command = e.raw_message.substring(6).split(' ');
+
+        for (let i = 0; i < command.length; i++) {
+            const attribute = command[i].split(':')[0];
+            const num = command[i].split(':')[1];
+
+            if (attribute === '6星') {
+                players[name].sixStars = parseInt(num);
+                players[name].prePulls = 0;
+            } else if (attribute === '5星') {
+                players[name].fiveStars = parseInt(num);
+            } else if (attribute === '总抽数') {
+                players[name].totalPulls = parseInt(num);
+            } else if (attribute === '今日抽数') {
+                players[name].dailyPulls = parseInt(num);
+            } else if (attribute === '垫刀') {
+                players[name].prePulls = parseInt(num);
+            }
+        }
+
+        players[name].probability = getProbability(players[name].prePulls);
+
+        const data = JSON.stringify(players, null, 2);
+        fs.writeFile(arknightsData, data, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log('JSON data is saved.');
+        });
+    }
 })
 
 interface IPlayers {
@@ -52,6 +101,26 @@ interface IPlayer {
     prePulls: number;
     dailyPulls: number;
     probability: string;
+}
+
+class Player {
+    name: string;
+    sixStars: number;
+    fiveStars: number;
+    totalPulls: number;
+    prePulls: number;
+    dailyPulls: number;
+    probability: string;
+
+    constructor(name: string) {
+        this.name = name;
+        this.sixStars = 0;
+        this.fiveStars = 0;
+        this.totalPulls = 0;
+        this.prePulls = 0; //指抽出货之前的垫刀，我实在不知道怎么翻译
+        this.dailyPulls = 0;
+        this.probability = '2%';
+    }
 }
 
 function getProbability(num: number): string {
--- src/plugin-setUpCharacters.ts
+++ src/plugin-setUpCharacters.ts
@@ -1,30 +1,15 @@
-import { segment } from 'oicq';
+import * as fs from 'fs';
 import { bot } from './plugin-bot';
 
-let players: IPlayers = {
-    "Template": [
-        {
-            "name": "Role",
-            "hp": 9,
-            "san": 11,
-            "magic": 11,
-            "mov": 10,
-            "力量": 50,
-            "体质": 55,
-            "体型": 65,
-            "敏捷": 45,
-            "外貌": 70,
-            "智力": 75,
-            "意志": 35,
-            "教育": 65,
-            "幸运": 75,
-        }
-    ]
-};
+let players: IPlayers = {};
+
+const playersData = 'data/playersData.json'
+
+initialize();
 
 bot.on('message', function (e) {
     if (e.raw_message === '.st' || e.raw_message === '。st') {
-        let reply = `属性记录：.st (del/clr/show) ([属性名]:[属性值])\n.st 力量:50 体质:55 体型:65 敏捷:45 外貌:70 智力:75 意志:35 教育:65 幸运:75\n.st hp-1 后接+/-时视为从原值上变化\n.st san+1d6 修改属性时可使用掷骰表达式\n.st del kp裁决 //删除已保存的属性\n.st clr //清空当前卡\n.st show 灵感 //查看指定属性\n.st show //无参数时查看所有属性，请使用只st加点过技能的半自动人物卡！\n部分COC属性会被视为同义词，如智力/灵感、理智/san、侦查/侦察`;
+        let reply = `属性记录：.st (del/clr/show) ([属性名]:[属性值])\n.st hp:9 san:11 magic:11 mov:10 力量:50 体质:55 体型:65 敏捷:45 外貌:70 智力:75 意志:35 教育:65 幸运:75\n.st del hp //删除已保存的属性\n.st clr //清空当前卡\n.st show 灵感 //查看指定属性\n.st show //无参数时查看所有属性，请使用只st加点过技能的半自动人物卡！`;
         e.reply(
             [
                 reply
@@ -55,17 +40,52 @@ bot.on('message', function (e) {
                     roleAttribute = roleAttribute.substring(4).replace(' ', '');
                     reply = `${roleAttribute}=${getSkill(character, roleAttribute)}`;
                 } else {
-                    reply = `${character.hp}\n${character.magic}\n${character.mov}\n`;
+                    reply = `hp:${character.hp}\nsan:${character.san}\nmagic:${character.magic}\nmov:${character.mov}\n力量:${character.力量}\n体质:${character.体质}\n体型:${character.体型}\n敏捷:${character.敏捷}\n外貌:${character.外貌}\n智力:${character.智力}\n意志:${character.意志}\n教育:${character.教育}\n幸运:${character.幸运}`;
                 }
             } else {
-                let roleAttributes = tags.replace(/[(\w+):]/ig, '').split(' ');
-                let attributeNums = tags.replace(/[:(\d+)]/ig, '').split(' ');
+                const data = tags.split(' ');
+                let roleAttributes: string[] = [];
+                let attributeNums: string[] = [];
+
+                for (let i = 0; i < data.length; i++) {
+                    const skill = data[i].split(':')[0];
+                    const num = data[i].split(':')[1];
+
+                    roleAttributes.push(skill);
+                    attributeNums.push(num);
+                }
+
                 reply = setUpSkills(character, roleAttributes, attributeNums);
             }
         } else {
+            let template: ICharacter = {
+                "hp": 9,
+                "san": 11,
+                "magic": 11,
+                "mov": 10,
+                "力量": 50,
+                "体质": 55,
+                "体型": 65,
+                "敏捷": 45,
+                "外貌": 70,
+                "智力": 75,
+                "意志": 35,
+                "教育": 65,
+                "幸运": 75,
+            };
+
+            players[name] = [template];
             reply = '未查询到卡片，正在初始化卡片...\n初始化卡片成功！';
-            players[name] = players.Template;
         }
+
+        const data = JSON.stringify(players, null, 2);
+
+        fs.writeFile(playersData, data, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log('JSON data is saved.');
+        });
 
         e.reply(
             [
@@ -85,6 +105,8 @@ function setUpSkills(character: ICharacter, skills: string[], numbers: string[])
             setUpSkill(character, skills[i], parseInt(numbers[i]));
             reply += `\n${skills[i]}:${numbers[i]}`;
         }
+
+        return reply;
     }
 
     return '抱歉博士，参数似乎并不匹配。请重新输入要分配的参数。';
@@ -92,8 +114,7 @@ function setUpSkills(character: ICharacter, skills: string[], numbers: string[])
 
 function setUpSkill(character: ICharacter, skill: string, num: number) {
     if (isValidKey(skill, character)) {
-        if (skill != 'name')
-            character[skill] = num;
+        character[skill] = num;
     }
 }
 
@@ -109,8 +130,26 @@ function isValidKey(key: string, object: ICharacter): key is keyof typeof object
     return key in object;
 }
 
+function initialize() {
+    fs.access(playersData, fs.constants.F_OK, (err) => {
+        if (err)
+            fs.writeFile(playersData, '{}', (err) => {
+                if (err) {
+                    throw err;
+                }
+            });
+
+        fs.readFile(playersData, 'utf-8', (err, data) => {
+            if (err) {
+                throw err;
+            }
+
+            players = JSON.parse(data.toString());
+        });
+    });
+}
+
 interface ICharacter {
-    name: string;
     hp: number;
     san: number;
     magic: number;
