b859fcfd59d6c18ea9e7e2a0064c447e36e1ce1f
AngelBeats-Kanade <angelbeats177@gamil.com>
2023-04-03 23:06:24 +0800

    Update login function

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import './plugin-request'; //加群和好友
 import './plugin-random'; //骰娘的功能
 import './plugin-pixiv'; //pixiv的功能
 import './plugin-online'; //监听上线事件
-import './plugin-arknights';//明日方舟相关插件
-import './plugin-setUpCharacters'; //设置卡片属性的功能
+// import './plugin-arknights';//明日方舟相关插件
+// import './plugin-setUpCharacters'; //设置卡片属性的功能
